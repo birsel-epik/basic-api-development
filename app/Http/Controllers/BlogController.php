f48e2cd550cd2e5792d37a79e3cffcbab68f34b7
birsel <birsel.epik@gmail.com>
2025-02-12 17:02:20 +0300

    feat(api-development): crud operations added

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function getAllBlogs()
+    {
+        return Blog::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function insertBlog(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        $blog = Blog::create([
+            'title' => $validatedData['title'],
+            'content' => $validatedData['content'],
+            'user_id' => auth()->id(),
+        ]);
+
+        return response()->json($blog, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function showBlog(string $id)
+    {
+        return $blog;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function updateBlog(Request $request, string $id)
+    {
+        $this->authorize('update', $blog);
+
+        $validatedData = $request->validate([
+            'title' => 'sometimes|string|max:255',
+            'content' => 'sometimes|string',
+        ]);
+
+        $blog->update($validatedData);
+
+        return response()->json($blog);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroyBlog(string $id)
+    {
+        $this->authorize('delete', $blog);
+
+        $blog->delete();
+
+        return response()->json(null, 204);
+    }
+}
---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+     /**
+     * Display a listing of the resource.
+     */
+    public function getAllComments()
+    {
+        return $blog->comments;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function insertComment(Request $request, Blog $blog)
+    {
+        $validatedData = $request->validate([
+            'content' => 'required|string',
+        ]);
+
+        $comment = $blog->comments()->create([
+            'content' => $validatedData['content'],
+            'user_id' => auth()->id(),
+        ]);
+
+        return response()->json($comment, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function showComment(Comment $comment)
+    {
+        return $comment;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function updateComment(Request $request, Comment $comment)
+    {
+        $this->authorize('update', $comment);
+
+        $validatedData = $request->validate([
+            'content' => 'sometimes|string',
+        ]);
+
+        $comment->update($validatedData);
+
+        return response()->json($comment);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroyComment(Comment $comment)
+    {
+        $this->authorize('delete', $comment);
+
+        $comment->delete();
+
+        return response()->json(null, 204);
+    }
+}
